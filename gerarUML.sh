877dd7df4461847eabea58eac233f1efd6145f1e
sergioh665 <sergioh665@gmail.com>
2024-12-09 11:31:37 +0000

    🔧CHORE: corrigindo arquivos para geracao de diagramas

--- gerarUML.sh
+++ gerarUML.sh
@@ -1,9 +1,3 @@
-PLANTUML_JAR="plantuml.jar"
-OUTPUT_DIR="diagrams" #saída do diagrama
+#!/bin/sh
 
-# Verifica se o plantuml.jar está presente
-if [ ! -f "$PLANTUML_JAR" ]; then
-    echo "Erro: Não foi encontrado o arquivo plantuml.jar!"
-    exit 1
-fi
-java -jar "$PLANTUML_JAR" -tpng -o "$OUTPUT_DIR" uml/todos.puml #gerar o diagrama
+java -jar plantuml.jar -tpng -o diagrams uml/todos.puml #gerar o diagrama
