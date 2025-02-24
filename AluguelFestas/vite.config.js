43d96543260f7a1a8f66accccd7c15febb71f7ac
sergioh665 <sergioh665@gmail.com>
2025-02-24 11:18:30 +0000

    🔧CHORE: criando certificado https

--- AluguelFestas/vite.config.js
+++ AluguelFestas/vite.config.js
@@ -1,22 +1,29 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
+import fs from 'fs';
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => ['md-linedivider'].includes(tag),
-        }
-      }
-    }),
+    vue(),
   ],
+  server: {
+    // Desative HTTPS temporariamente
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, 'localhost.key')),
+    //   cert: fs.readFileSync(path.resolve(__dirname, 'localhost.crt'))
+    // },
+    port: 3000, // Defina a porta desejada aqui
+    open: true, // Abre o navegador automaticamente
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
